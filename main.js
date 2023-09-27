b2713ce9cd0c2163fdb3923bf93987a91505623c
victor <2006victorlegal@gmail.com>
2023-09-27 08:54:15 -0300

    Fazendo ajustes.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const operacoes = {
   ADICAO: '+',
   SUBTRACAO: '-',
   MULTIPLICACAO: 'x',
-  DIVISAO: '/'
+  DIVISAO: '÷'
 }
 
 // Classe Calculadora ---------------------------------------------------------------------------
@@ -47,7 +47,7 @@ class Calculadora {
         campoDeExibicao.innerHTML = `O número ${this._valorA} não pode ser divido por ${this._valorB}`
         setTimeout(() => {campoDeExibicao.innerHTML = `${this._valorA += '  ' + this._operacao+ '  '}`}, 1400);
       }else { return this._valorA / this._valorB }
-      default: throw new Error ('FAÇA UMA OPERAÇÃO');
+      default: if (this.valorA == undefined || this.valorB == undefined || this.operacao == undefined) { throw new Error('faça uma operação');}
     }
   }
 }
@@ -56,15 +56,16 @@ const campoDeExibicao = document.querySelector('.display');//campo do display da
 const botoesOperadores = document.querySelectorAll('.operator');//botões de operação como: +, -, x e / --------------------------
 const botoesNumeradosEVirgula = document.querySelectorAll('.operator_');//botões de numeros do 0 ao 9 e a virgula(,) ---------------------------
 const botaoIgual = document.getElementById('igual');//botão para exibir o resultado de toda a operação, o igual(=) ------------------------------------
+const aviso = document.querySelector('.aviso')
 
 //exbir no campo ------------------------------------------------------
+const operadores = ['+', '-', 'x', '÷'];
 botoesOperadores.forEach((botao) => {
   botao.addEventListener("click", () => {
     const conteudoCampo = campoDeExibicao.innerHTML.trim(); 
     const ultimoCaractere = conteudoCampo.slice(-1);
-
     if (operadores.includes(ultimoCaractere) && operadores.includes(botao.innerHTML)) {
-      campoDeExibicao.innerHTML = conteudoCampo.slice(0, -1) + '  ' + botao.innerHTML + '  ';
+      campoDeExibicao.innerHTML = conteudoCampo.slice(0, -1) + botao.innerHTML + ' ' ;
     } else {
       campoDeExibicao.innerHTML += '  ' + botao.innerHTML + '  ';
     }
@@ -90,21 +91,21 @@ const botaoLimpar = document.getElementById('limpar');
 botaoLimpar.addEventListener("click", () => {campoDeExibicao.innerHTML = ""});
 
 //fazer o calculo e exibir resultado da conta ----------------------------------------------
-const resultado = new Calculadora();
-const operadores = ['+', '-', 'x', '/'];
+const calcular = new Calculadora();
+
 botaoIgual.addEventListener("click", () => {
   const operacao = campoDeExibicao.innerHTML.split('').find(operar => operadores.includes(operar));
   const [valorA, valorB] = campoDeExibicao.innerHTML.split(operacao).map(item => item.replace(',', '.'));
 
-  resultado.valorA = parseFloat(valorA);
-  resultado.operacao = operacao;
-  resultado.valorB = parseFloat(valorB);
+  calcular.valorA = parseFloat(valorA);
+  calcular.operacao = operacao;
+  calcular.valorB = parseFloat(valorB);
 
-  if(isNaN(resultado.conta())){
+  if(isNaN(calcular.conta())){
     console.log('erro ao fazer a operação (operação não suportada).');
     campoDeExibicao.innerHTML = `erro ao fazer a operação.`
     setTimeout(()=> {campoDeExibicao.innerHTML = ''}, 1200);
     return
-  }else { console.log("Operação da conta:", valorA, operacao, valorB, "=", resultado.conta()); }
-  campoDeExibicao.innerHTML = resultado.conta().toString().replace('.', ',');
+  }else { console.log("Operação da conta:", valorA, operacao, valorB, "=", calcular.conta()); }
+  campoDeExibicao.innerHTML = calcular.conta().toString().replace('.', ',');
 });
